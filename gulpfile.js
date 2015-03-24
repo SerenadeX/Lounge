c89678baf02c4a5eb99bd8290ddb6496cc144edc
Rhett Rogers <serenade.xs@icloud.com>
2015-03-23 20:07:16 -0600

    Setup stuff for customizing lounge with sass

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  gulp.src('./assets/sass/style.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./assets/css'));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('./assets/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
